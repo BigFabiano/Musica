dacf006853d720ee5adb504f6c06c51e6d5e9474
FabianoEtec <nbscbs@gmail.com>
2021-03-06 22:27:28 -0300

    Finalizei a letra da musica

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep();
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Title = "Letra musica RARIDADE";
+            Console.WriteLine(@"
+            █████████████████████████████████████████████████████████████
+            █                 Raridade - Anderson Freire                █
+            █████████████████████████████████████████████████████████████
+            ░                                                           ░
+            ░   Não consigo ir além do Teu olhar                        ░ 
+            ░   Tudo que eu consigo é imaginar                          ░
+            ░   A riqueza que existe dentro de você                     ░
+            ░                                                           ░
+            ░   O ouro eu consigo só admirar                            ░
+            ░   Mas Te olhando eu posso a Deus adorar                   ░
+            ░   Sua alma é o bem que nunca envelhecerá                  ░
+            ░                                                           ░
+            ░   O pecado não consegue esconder                          ░
+            ░   A marca de Jesus que existe em você                     ░
+            ░   O que você fez ou deixou de fazer                       ░
+            ░   Não mudou o ínicio, Deus escolheu você                  ░
+            ░   Sua raridade não está naquilo que você possui           ░
+            ░   Ou que sabe fazer                                       ░
+            ░   Isso é mistério de Deus com você                        ░
+            ░                                                           ░
+            ░   Você é um espelho que reflete a imagem do Senhor        ░
+            ░   Não chore se o mundo ainda não o notou                  ░
+            ░   Já é o bastante Deus reconhecer o seu valor             ░
+            ░   Você é precioso, mais raro que o ouro puro de Ofir      ░
+            ░   Se você desistiu, Deus não vai desistir                 ░
+            ░   Ele está aqui pra te levantar se o mundo te fizer cair  ░
+            ░                                                           ░");                                                        
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(@"
+
+            ░   O ouro eu consigo só admirar                            ░
+            ░   Mas te olhando eu posso a Deus adorar                   ░
+            ░   Sua alma é o bem que nunca envelhecerá                  ░
+            ░                                                           ░
+            ░   O pecado não consegue esconder                          ░
+            ░   A marca de Jesus que existe em você                     ░
+            ░   O que você fez ou deixou de fazer                       ░
+            ░   Não mudou o ínicio Deus escolheu você                   ░
+            ░   Sua raridade não está naquilo que você possui           ░
+            ░   Ou que sabe fazer                                       ░
+            ░   Isso é mistério de Deus com você                        ░
+            ░                                                           ░
+            ░   Você é um espelho que reflete a imagem do Senhor        ░
+            ░   Não chore se o mundo ainda não o notou                  ░
+            ░   Já é o bastante Deus reconhecer o seu valor             ░
+            ░   Você é precioso, mais raro que o ouro puro de Ofir      ░
+            ░   Se você desistiu, Deus não vai desistir                 ░
+            ░   Ele está aqui pra te levantar se o mundo te fizer cair  ░
+            ░                                                           ░
+            ░   Você é um espelho que reflete a imagem do Senhor        ░
+            ░   Não chore se o mundo ainda não o notou                  ░
+            ░   Já é o bastante Deus reconhecer o seu valor             ░
+            ░   Já é o bastante Deus reconhecer o seu valor             ░
+            ░   Você é precioso, mais raro que o ouro puro de Ofir      ░
+            ░   Se você desistiu, Deus não vai desistir                 ░
+            ░   Ele está aqui pra te levantar se o mundo te fizer cair  ░
+            ░                                                           ░
+            ░   Você é um espelho que reflete a imagem do Senhor        ░
+            ░   Não chore se o mundo ainda não o notou                  ░
+            ░   Já é o bastante Deus reconhecer o seu valor             ░
+            ░   Você é precioso, mais raro que o ouro puro de Ofir      ░
+            ░   Se você desistiu, Deus não vai desistir                 ░
+            ░   Ele está aqui pra te levantar se o mundo te fizer cair  ░
+            ░                                                           ░
+            █████████████████████████████████████████████████████████████");
+            Console.ReadKey();
+            Console.ResetColor();
+            Console.Clear();
+        }    
+    }
+}
